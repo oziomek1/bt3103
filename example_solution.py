b5987a8e138e4a6048760c5438f4d3a2f618e2ba
oziomek1 <ozimekwojciech@zoho.eu>
2019-10-29 12:55:11 +0800

    add example

---
+++ example_solution.py
@@ -0,0 +1,8 @@
+"""
+Input your code here:
+"""
+class Square:
+    def __init__(self, cell_size, x_position, y_position):
+        self.cell_size = cell_size
+        self.x_position = x_position
+        self.y_position = y_position
