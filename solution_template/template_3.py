d96fe75880c24373dde9c7bc6325cb4cf74703e3
oziomek1 <ozimekwojciech@zoho.eu>
2019-11-04 11:47:48 +0800

    upgrade exerices texts and about us section

---
+++ solution_template/template_3.py
@@ -0,0 +1,5 @@
+class Snake:
+  
+  def __init__(self, length):
+    ##START CODE HERE. DO NOT FORGET THE getLength METHOD##
+    
---
+++ solution_template/template_6.py
@@ -0,0 +1,7 @@
+class Snake():
+    def movement(self):
+        return "I move in straight lines"
+
+class badSnake():
+##START CODE HERE##
+
